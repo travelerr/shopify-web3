bd503c7541431ef7666513ef21992ae24ae7a2fc
Justin <foliofinance@gmail.com>
2022-07-04 19:26:31 -0400

    initial commit

---
+++ dawn/scripts/cart-icon.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import { CartIcon } from "./Components/CartIcon";
+
+const rootEl = document.getElementById("react-icon-cart");
+
+rootEl && ReactDOM.render(rootEl);
